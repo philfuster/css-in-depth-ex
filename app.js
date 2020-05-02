106031e64156b5191306f33b39b7ff908e0e5af6
PhilFuster <pafus23@protonmail.com>
2020-05-02 02:23:23 -0400

    This commit is for Chapter 3 and its listings. I also edited the index page to describe what I have going on.
    I have a route for /
    This route will take the path and try to render it.
    EX:
      localhost:3000/listing3-10
    
    Will take everything after the / (aka the path)
    And attempt to render a file with that name.
    EX: listing3-10.ejs
    
    This makes it so I can create new HTML files on the fly as I read a chapter, create
    a CSS file to go with it, and be able to access it via the name of the file right away instead
    of having to create a new route for it.
    
    Not sure if this will work the whole way for the book, like maybe the listings will get more
    complex. We shall see.

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -18,9 +17,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -35,7 +34,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.sendFile(path.join(__dirname, 'views/error.html'));
+  res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,36 @@ const express = require('express');
 const router = express.Router();
 
 const path = require('path');
+
+const fs = require('fs');
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.sendFile(path.join(__dirname, '../views/index.html'));
+  res.render('index');
+});
+
+/* GET home page. */
+/**
+ * This router is used so I can quickly create html files
+ * and access them like a filesystem from the browser without having
+ * to create a handler for each new file specifically.
+ */
+router.get(/\w*/g, function (req, res, next) {
+  const pathName = req.path;
+  const fileName = pathName.substr(1);
+  console.log('yikes: ' + fileName);
+  res.render(fileName);
+  // try {
+  //   if (fs.existsSync(path.join(__dirname, `../views/${fileName}`))) {
+  //     console.log('exists');
+  //     res.sendFile(path.join(__dirname, '../views/', fileName));
+  //   } else {
+  //     console.log(`File doesn't exist.. ${fileName}`);
+  //     res.sendFile(path.join(__dirname, '../views/', '404.html'));
+  //   }
+  // } catch (error) {
+  //   console.log(error.stack);
+  // }
 });
 
 module.exports = router;
