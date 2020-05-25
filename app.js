b4c1891c902f1a6407d0e71b410e07305252a932
PhilFuster <pafus23@protonmail.com>
2020-05-25 04:32:52 -0400

    Chapters 8 and 9.

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const debug = require('debug');
+
+// Binding debug's output to the console.
+// Initializing Loggers
+debug.log = console.log.bind(console);
+const log = debug('css-in-depth:app');
+const error = debug('css-in-depth: error');
+
 var indexRouter = require('./routes/index');
 
 var app = express();
@@ -17,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 app.use('/', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,28 @@ const router = express.Router();
 
 const path = require('path');
 
+const debug = require('debug');
+
+const log = debug('css-in-depth:index');
+
 const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index');
+  const model = {};
+  let arrayOfFiles;
+  try {
+    arrayOfFiles = fs.readdirSync(path.join(__dirname, '../views'));
+    // filter out any views not pertaining to listings
+    arrayOfFiles = arrayOfFiles.filter((fileName) => {
+      const pattern = /^listing/g;
+      return pattern.test(fileName);
+    });
+    log(arrayOfFiles);
+  } catch (err) {
+    log(`${err.stack}`);
+  }
+  res.render('index', { listings: arrayOfFiles });
 });
 
 /* GET home page. */
@@ -20,19 +37,8 @@ router.get('/', function (req, res, next) {
 router.get(/\w*/g, function (req, res, next) {
   const pathName = req.path;
   const fileName = pathName.substr(1);
-  console.log('yikes: ' + fileName);
+  log(`yikes: ${fileName}`);
   res.render(fileName);
-  // try {
-  //   if (fs.existsSync(path.join(__dirname, `../views/${fileName}`))) {
-  //     console.log('exists');
-  //     res.sendFile(path.join(__dirname, '../views/', fileName));
-  //   } else {
-  //     console.log(`File doesn't exist.. ${fileName}`);
-  //     res.sendFile(path.join(__dirname, '../views/', '404.html'));
-  //   }
-  // } catch (error) {
-  //   console.log(error.stack);
-  // }
 });
 
 module.exports = router;
